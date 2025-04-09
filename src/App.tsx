150ef0db69f8da39b1e245afc8a9cad2cdf06a68
Guru Prathosh <86939870+iamprathosh@users.noreply.github.com>
2025-04-10 01:52:39 +0530

    Fix /network and /storage routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { useEffect } from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
 import NetworkPage from "./pages/NetworkPage";
 import StoragePage from "./pages/StoragePage";
@@ -11,13 +12,19 @@ import DatabaseWarning from "./components/DatabaseWarning";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <DatabaseWarning />
-      <BrowserRouter>
+const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log("Current pathname:", location.pathname);
+  }, [location.pathname]);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <DatabaseWarning />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/network" element={<NetworkPage />} />
@@ -25,9 +32,15 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
+
+const WrappedApp = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
-export default App;
+export default WrappedApp;
